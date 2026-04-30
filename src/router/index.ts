d789504a7e913a8849eb52b890af1461336791e9
Cnkrru <J19323850@gmail.com>
2026-04-30 22:40:57 +0800

    113

---
+++ src/router/index.ts
@@ -0,0 +1,91 @@
+import { createRouter, createWebHistory, createMemoryHistory, Router, RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Index',
+        component: () => import('../pages/Index.vue')
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: () => import('../pages/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import('../pages/About/About.vue')
+    },
+    {
+        path: '/archives',
+        name: 'Archives',
+        component: () => import('../pages/Archives.vue')
+    },
+    {
+        path: '/links',
+        name: 'Links',
+        component: () => import('../pages/Links.vue')
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        component: () => import('../pages/Projects.vue')
+    },
+    {
+        path: '/post/:id',
+        name: 'Post',
+        component: () => import('../pages/post/Posts.vue')
+    },
+    {
+        path: '/project/:id',
+        name: 'Project',
+        component: () => import('../pages/project/Projects.vue')
+    },
+    {
+        path: '/changelog',
+        name: 'Changelog',
+        component: () => import('../pages/log/Changelog.vue')
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('../pages/NotFound.vue')
+    }
+]
+
+const createAppHistory = (): ReturnType<typeof createWebHistory> | ReturnType<typeof createMemoryHistory> => {
+    if (import.meta.env.SSR) {
+        return createMemoryHistory()
+    } else {
+        return createWebHistory()
+    }
+}
+
+type ScrollBehaviorReturn = void | boolean | {
+    left: number
+    top: number
+    behavior: ScrollBehavior
+}
+
+const scrollToTop = (): void => {
+    if (typeof document !== 'undefined') {
+        const el = document.querySelector('.center-card-content')
+        if (el) {
+            el.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        }
+    }
+}
+
+export function createAppRouter(): Router {
+    const router = createRouter({
+        history: createAppHistory(),
+        routes,
+        scrollBehavior(): void { scrollToTop() }
+    })
+    return router
+}
+
+export default createAppRouter
